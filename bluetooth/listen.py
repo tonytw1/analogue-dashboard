ea1978d8e28d61b372593b238af0e1983fe843ba
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-14 14:43:27 +0100

    Spike - echo signal strength of observed bluetooth LE devices into the metrics bus

--- bluetooth/listen.py
+++ bluetooth/listen.py
@@ -2,6 +2,13 @@
 
 # TODO Using passive scans only
 from bluepy.btle import Scanner, DefaultDelegate
+import paho.mqtt.client as mqtt
+import re
+
+mqtt_topic = 'metrics'
+
+mqtt_client = mqtt.Client()
+mqtt_client.connect("10.0.45.11", 32183, 60)
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -9,9 +16,13 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
 	print "Received new data from", dev.addr, dev.getScanData(), dev.rssi, dev.addrType, dev.getValueText(dev.addrType), dev.updateCount
+	short_addr = re.sub(':', '', dev.addr)
+	message = short_addr + ":" + str(dev.rssi)
+	mqtt_client.publish(mqtt_topic, message)
 
-scanner = Scanner().withDelegate(ScanDelegate())
+scanner = Scanner(0).withDelegate(ScanDelegate())
 scanner.start()
 while True:
     print "Still running..."
     scanner.process()
+
