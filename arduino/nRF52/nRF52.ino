ec101cdc38d14e968f974c22f74edbe5ab5891a0
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-17 09:37:32 +0100

    Clean up; remove example code we are not using

--- arduino/nRF52/nRF52.ino
+++ arduino/nRF52/nRF52.ino
@@ -1,74 +1,26 @@
-/*********************************************************************
- This is an example for our nRF52 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
 #include <bluefruit.h>
-
-// Beacon uses the Manufacturer Specific Data field in the advertising
-// packet, which means you must provide a valid Manufacturer ID. Update
-// the field below to an appropriate value. For a list of valid IDs see:
-// https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers
-// 0x004C is Apple (for example)
-#define MANUFACTURER_ID   0x004C 
-
-// AirLocate UUID: E2C56DB5-DFFB-48D2-B060-D0F5A71096E0
-uint8_t beaconUuid[16] = 
-{ 
-  0xE2, 0xC5, 0x6D, 0xB5, 0xDF, 0xFB, 0x48, 0xD2, 
-  0xB0, 0x60, 0xD0, 0xF5, 0xA7, 0x10, 0x96, 0xE0, 
-};
-
-// A valid Beacon packet consists of the following information:
-// UUID, Major, Minor, RSSI @ 1M
-BLEBeacon beacon(beaconUuid, 0x0000, 0x0000, -54);
 
 int count = 0;
 
 void setup() 
 {
-  Serial.begin(115200);
-
-  Serial.println("Bluefruit52 Beacon Example");
-  Serial.println("--------------------------\n");
-
   Bluefruit.begin();
 
   // off Blue LED for lowest power consumption
   Bluefruit.autoConnLed(false);
   
   // Set max power. Accepted values are: -40, -30, -20, -16, -12, -8, -4, 0, 4
-  Bluefruit.setTxPower(4);
-  // Bluefruit.setName("Bluefruit52");
-
-  // Manufacturer ID is required for Manufacturer Specific Data
-  beacon.setManufacturer(MANUFACTURER_ID);
-
+  Bluefruit.setTxPower(0);
+  
   // Setup the advertising packet
   startAdv();
-
-  Serial.println("Broadcasting beacon, open your beacon app to test");
-
-  // Suspend Loop() to save power, since we didn't have any code there
-  // suspendLoop();
 }
-
 
 bool setAdvertisingData(BLEAdvertising& adv, int count)
 {
-  adv.clearData();
-
   struct ATTR_PACKED
   {
-    int8_t   count;
+    int8_t count;
   } beacon_data =
   {
       .count = count
@@ -76,19 +28,14 @@ bool setAdvertisingData(BLEAdvertising& adv, int count)
 
   VERIFY_STATIC(sizeof(beacon_data) == 1);
 
-
+  adv.clearData();
   adv.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   return adv.addData(BLE_GAP_AD_TYPE_MANUFACTURER_SPECIFIC_DATA, &beacon_data, sizeof(beacon_data));
 }
 
 void startAdv(void)
-{  
-
-  //Bluefruit.Advertising.setBeacon(beacon);
+{
   setAdvertisingData(Bluefruit.Advertising, count);
-  // Secondary Scan Response packet (optional)
-  // Since there is no room for 'Name' in Advertising packet
-  Bluefruit.ScanResponse.addName();
   
   /* Start Advertising
    * - Enable auto advertising if disconnected
@@ -118,4 +65,3 @@ void loop()
   setAdvertisingData(Bluefruit.Advertising, count);
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
 }
-
