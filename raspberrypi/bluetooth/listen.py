6c80dbb754486c954fdb74b2c91e24b91993fd3b
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-20 09:58:40 +0100

    Whitelist interesting devices

--- raspberrypi/bluetooth/listen.py
+++ raspberrypi/bluetooth/listen.py
@@ -3,6 +3,7 @@ from bluepy.btle import Scanner, DefaultDelegate, ScanEntry
 import paho.mqtt.client as mqtt
 import re
 
+devices = ['f2:67:bf:fc:aa:98']
 mqtt_topic = 'metrics'
 
 mqtt_client = mqtt.Client()
@@ -14,13 +15,14 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
 	print "Received new data from", dev.addr, dev.getScanData(), dev.rssi, dev.addrType, dev.getValueText(dev.addrType), dev.updateCount
-	short_addr = re.sub(':', '', dev.addr)
-	data_items = dev.getScanData()
-	manufacturer_specific_data_item = filter(lambda x: x[0] == ScanEntry.MANUFACTURER, data_items)
-	if (len(manufacturer_specific_data_item) > 0):
-		value = manufacturer_specific_value = manufacturer_specific_data_item[0][2]
-		message = short_addr + ":" + value
-		mqtt_client.publish(mqtt_topic, message)
+	if (dev.addr in devices):
+		short_addr = re.sub(':', '', dev.addr)
+		data_items = dev.getScanData()
+		manufacturer_specific_data_item = filter(lambda x: x[0] == ScanEntry.MANUFACTURER, data_items)
+		if (len(manufacturer_specific_data_item) > 0):
+			value = manufacturer_specific_value = manufacturer_specific_data_item[0][2]
+			message = short_addr + ":" + value
+			mqtt_client.publish(mqtt_topic, message)
 
 scanner = Scanner().withDelegate(ScanDelegate())
 scanner.start(passive=True)
