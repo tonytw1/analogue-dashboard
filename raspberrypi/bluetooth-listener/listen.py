86c173ee556b902b2fa23cca6121f7c5adee7ee3
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-10-06 18:37:52 +0100

    Decode struct encoded payload into count and duration messages

--- raspberrypi/bluetooth-listener/listen.py
+++ raspberrypi/bluetooth-listener/listen.py
@@ -2,6 +2,8 @@
 from bluepy.btle import Scanner, DefaultDelegate, ScanEntry
 import paho.mqtt.client as mqtt
 import re
+import binascii
+import struct
 
 devices = ['f2:67:bf:fc:aa:98']
 mqtt_topic = 'metrics'
@@ -20,9 +22,20 @@ class ScanDelegate(DefaultDelegate):
 		data_items = dev.getScanData()
 		manufacturer_specific_data_item = filter(lambda x: x[0] == ScanEntry.MANUFACTURER, data_items)
 		if (len(manufacturer_specific_data_item) > 0):
-			value = manufacturer_specific_value = manufacturer_specific_data_item[0][2]
-			message = short_addr + ":" + value
-			mqtt_client.publish(mqtt_topic, message)
+			value = manufacturer_specific_data_item[0][2]
+			count_hex = value[0:4]
+			count_bytes = binascii.unhexlify(count_hex)
+			count = struct.unpack('H', count_bytes)[0]
+
+			pulse_duration_hex = value[4:12]
+			pulse_duration_bytes = binascii.unhexlify(pulse_duration_hex)
+			pulse_duration = struct.unpack('L', pulse_duration_bytes)[0]
+
+			count_message = short_addr + "count:" + str(count)
+			mqtt_client.publish(mqtt_topic, count_message)
+
+			pulse_duration_message = short_addr + "pulseduration:" + str(pulse_duration)
+			mqtt_client.publish(mqtt_topic, pulse_duration_message)
 
 scanner = Scanner().withDelegate(ScanDelegate())
 scanner.start(passive=True)
